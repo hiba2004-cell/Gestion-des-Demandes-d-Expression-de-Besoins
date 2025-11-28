f3e97a43440eeef5fa8f801cf6d242d97ecfdb66
hiba2004-cell <hiba.nadiri04@gmail.com>
2025-11-28 12:49:40 +0100

    deconnexion

--- logout.php
+++ logout.php
@@ -1,5 +1,12 @@
 <?php
-session_unset();
+session_start();
+
+// Détruire toutes les variables de session
+$_SESSION = [];
+
+// Détruire la session
 session_destroy();
+
+// Rediriger vers la page de connexion ou l'accueil
 header("Location: index.php");
-exit();
+exit;
